1f14cd39298ca80735325607397401bf9d07d9c4
8977769080 <51908261+8977769080@users.noreply.github.com>
2022-03-24 11:33:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.94.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {
-    
-  }
-}
+
 resource "azurerm_virtual_network" "vnet1" {
   name                = "${var.vnetname}"
   resource_group_name = "${var.rgname}"
   location            = "${var.location}"
   address_space       = ["${var.address_space}"]
+  tags                = var.tags
 }
